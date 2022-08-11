8a863c6b61a0f9596a2432581fd22142784c4864
Fernando Monnot <fermonnot@gmail.com>
2022-08-10 23:58:26 -0400

    my second commit

--- src/js/component/Counter.jsx
+++ src/js/component/Counter.jsx
@@ -1,20 +1,28 @@
 
 import React from "react";
-import PropTypes from "prop-types"
+import PropTypes from "prop-types";
+import "../../styles/index.css";
+
+
+
+
 const Counter =(props) => {
     const {one, two, three, four}= props
     return (
-   <div className="container">
-    <div className="row">
+   <div className="container text-center mt-3 ">
+    <h2 className="text">It's Time to do it!</h2>
+
+    <div className="row counter justify-content-center">
         <div className="col-2">{<i className="fas fa-clock"></i>}</div>
         <div className="col-2">{four}</div>
         <div className="col-2">{three}</div>
         <div className="col-2">{two}</div>
         <div className="col-2">{one}</div>
+        
     </div>
    </div>
-        )
-}
+    );
+};
 Counter.propTypes ={
     one: PropTypes.number ,
     two: PropTypes.number ,
